5bb96ea01f294b399e56acd361e64841aefcda40
Cameron Karagitz <ckaragitz@salesforce.com>
2020-05-16 12:47:28 -0400

    file stuff

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+import gunicorn
 import requests
 import os
-import gunicorn
+import io
+import csv
 import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/leads')
-def get_leads():
-
-    data = []
+@app.route('/leads', methods=["POST"])
+def post_leads():
 
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
 
-    print("Querying Postgres...")
-    cur.execute('SELECT * FROM "External Lead"', cur.rowcount)
-    rows = cur.fetchall()
+    f = request.files['file']
+    if not f:
+        return "No file"
 
-    for row in rows:
-
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    #print("file contents: ", file_contents)
+    #print(type(file_contents))
+    print(csv_input)
+    for row in csv_input:
         print(row)
-        data.append(row)
 
-    return jsonify(data=data)
+    stream.seek(0)
+    result = stream.read()
+
+    response = make_response(result)
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
